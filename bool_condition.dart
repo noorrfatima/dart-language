326122ab3eb0d8a8b4abf96982ca0137983df62c
noorrfatima <noorrfatiima@gmail.com>
2023-07-29 16:57:49 +0500

    dart language

---
+++ bool_condition.dart
@@ -0,0 +1,25 @@
+void main() {
+  // int abc = 34;
+  // int a = 21;
+  // bool w = abc > a && !(a < abc);
+  // //       true && !(true)
+  // //       true && false
+  // //       false
+  // print(w);
+  // print(w);
+
+  // // String email = "abc@gmail.com";
+  // // String password = "12345678";
+  // // bool condition = email == "abc@gmail.com";
+  // // print(condition);
+
+  if (12 > 8) {
+    int num1 = 12;
+    int num2 = 10;
+    if (num1 > num2) {
+      print("num1 is greater than num2");
+    }
+  } else {
+    print("num2 is greater than num1");
+  }
+}
